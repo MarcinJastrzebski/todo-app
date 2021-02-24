2c362d35861308c5605425882544791e6f2b3b2c
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-24 18:16:52 +0100

    added a readAllTasks method to the controller

--- src/main/java/io/github/marcinjastrzebski/controller/TaskController.java
+++ src/main/java/io/github/marcinjastrzebski/controller/TaskController.java
@@ -4,6 +4,8 @@ import io.github.marcinjastrzebski.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RepositoryRestController
 class TaskController {
@@ -12,5 +14,11 @@ class TaskController {
 
     public TaskController(TaskRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<?> readAllTasks(){
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
     }
 }
