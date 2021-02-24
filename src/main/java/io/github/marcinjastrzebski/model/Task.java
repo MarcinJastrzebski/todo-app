77c02188ea5a5bacd7d13f2ff3235dc3ccf4f257
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-24 10:59:14 +0100

    added TaskRepository and prepared Task class

--- src/main/java/io/github/marcinjastrzebski/model/Task.java
+++ src/main/java/io/github/marcinjastrzebski/model/Task.java
@@ -1,4 +1,40 @@
 package io.github.marcinjastrzebski.model;
 
-public class Task {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private boolean done;
+
+    public Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
---
+++ src/main/java/io/github/marcinjastrzebski/model/TaskRepository.java
@@ -0,0 +1,9 @@
+package io.github.marcinjastrzebski.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+}
