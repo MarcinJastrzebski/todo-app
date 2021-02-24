4528c43a8de588cd4174058bc80b211b559c7be6
Marcin Jastrzebski <marcin.jastrzebski@outlook.com>
2021-02-24 22:19:34 +0100

    cleaning

--- src/main/java/io/github/marcinjastrzebski/controller/TaskController.java
+++ src/main/java/io/github/marcinjastrzebski/controller/TaskController.java
@@ -1,11 +1,10 @@
 package io.github.marcinjastrzebski.controller;
 
 import io.github.marcinjastrzebski.model.Task;
-import io.github.marcinjastrzebski.model.TaskRepository;
+import io.github.marcinjastrzebski.model.SqlTaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +14,9 @@ import java.util.List;
 @RestController
 class TaskController {
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
-    private final TaskRepository repository;
+    private final SqlTaskRepository repository;
 
-    public TaskController(TaskRepository repository) {
+    public TaskController(SqlTaskRepository repository) {
         this.repository = repository;
     }
 
